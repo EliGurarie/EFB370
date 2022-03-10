d89f7f002d05f4a7aa342112ffab37a9c413d751
Elie Gurarie <egurarie@umd.edu>
2022-03-09 20:20:46 -0500

    Uploaded Lab 7

--- shinyapps/03_AgeStructuredGrowth/agestructuredgrowth.R
+++ shinyapps/03_AgeStructuredGrowth/agestructuredgrowth.R
@@ -8,80 +8,68 @@ require(gplots)
 require(kableExtra)
 require(plyr)
 require(magrittr)
+require(shinyMatrix)
+require(utils)
 
 
-runPop <- function(n.stages, births, survival, init, tmax){
+runPop <- function(lifehistory, tmax){
     
-    births <- as.numeric(unlist(strsplit(births,",")))
-    survival <- as.numeric(unlist(strsplit(survival,",")))
-    init <- as.numeric(unlist(strsplit(init,",")))
+    n.stages <- nrow(lifehistory)
+    births <- lifehistory[,"births"]
+    survival <- lifehistory[,"survival"]
     
     M <- matrix(0, nrow = n.stages, ncol = n.stages)
     M[1,] <- births
     for(i in 2:n.stages) M[i,i-1] <- survival[i-1]
-        
-    time <- 1:tmax
-    pop <- sapply(time, function(t) {(M%^%t) %*% init})
-    return(pop)
+    row.names(M) <- row.names(lifehistory)
+    
+    time <- 0:tmax
+    pop <- sapply(time, function(t) {(M%^%t) %*% lifehistory[,"start"]})
+    row.names(pop) <- row.names(lifehistory) 
+    
+    return(list(pop = pop, leslie = M))
 }
 
-getLifeHistoryTable <- function(n.stages, births, survival){
-    births <- as.numeric(unlist(strsplit(births,",")))
-    survival <- as.numeric(unlist(strsplit(survival,",")))
-    data.frame(stage = 1:n.stages, births, survival) %>% mutate(
-        cum.survival = cumprod(survival),
-        
-    )
-}
-
+m <-cbind(c(20,0,0),c(.5,1,0),c(0,1.5,.5))
+colnames(m) <- c("start", "survival", "births")
+row.names(m) <- as.roman(1:3) %>% as.character
+ 
 ui <- fluidPage(
     sidebarPanel(
         h4("Life history parameters:"),
-        numericInput("n.stages", label = "Number of stages", value = 3, min = 1, step = 1),
-        textInput('births', 'Fecundity at stage: (separate by comma)', '0,1.5,.5'),
-        textInput('survival', 'Survival at stage: ', '.5,1,0'),
-        textInput('init', 'Initial distribution: ', '20,0,0'),
+        matrixInput(
+            "lifehistory",
+            value = m,
+            rows = list(extend = TRUE, 
+                        names = TRUE, 
+                        editableNames = TRUE, 
+                        delta = 2,
+                        delete = TRUE),
+            cols = list(names = TRUE, delta = 1),
+            class = "numeric"),
+            
         numericInput(inputId = "tmax", 
                      label = "Duration:", 
                      value = 20),
-        h2(actionButton("go", "Run simulation"))),
+        h3(actionButton("button", "Update Life Table")),
+        h3(actionButton("go", "Run Simulation"))),
     
     mainPanel(
         plotOutput("structuredgrowth", width = "100%", height = "350px"),
-        h3("Life History Table"),
-        tableOutput("lifehistorytable")
+        h3("Leslie Matrix"),
+        tableOutput("lesliematrix")
     )
 )
 
 server <- function(input, output) {
     sim <- eventReactive(input$go,
-                         runPop(input$n.stages, 
-                                input$births, 
-                                input$survival, 
-                                input$init, 
+                         runPop(input$lifehistory, 
                                 input$tmax))
     
     output$structuredgrowth = renderPlot({
         
-        births <- as.numeric(unlist(strsplit(input$births,",")))
-        survival <- as.numeric(unlist(strsplit(input$survival,",")))
-        init <- as.numeric(unlist(strsplit(input$init,",")))
-        n.stages <- input$n.stages
-        
-        partest <- c(length(input$births), length(survival), length(init), n.stages)
-        validate(need(length(births) == n.stages, 
-                      paste("Birth vector must have", n.stages, "elements.")),
-                 need(length(survival) >= n.stages-1, 
-                      paste("Survival vector must have at least", n.stages-1, "elements.")),
-                 need(length(init) == n.stages, 
-                      paste("Initial age stage distribution must have", n.stages, "elements.")))
-                 
-            
-        # !all(outer(partest, partest, Vectorize(identical))),
-        
-        
-        # plotting
-        pop <- sim()
+        mysim <- sim()
+        pop <- mysim$pop
         
         palette(rich.colors(nrow(pop)))
         par(mfrow = c(1,2), tck = 0.01, bty = "l", las= 1, 
@@ -90,19 +78,17 @@ server <- function(input, output) {
         layout(t(1:2), widths = c(2,1))
         
         matplot(t(pop), type = "o", pch = 19, lty = 1, xlab = "time", ylab = "N")
-        legend("top", col = 1:n.stages, ncol = ceiling(n.stages*2/3), 
-               lty = 1, legend = 1:n.stages, title = "age class")
         
-        barplot(pop[,ncol(pop)], col = 1:n.stages, 
+        legend("top", col = 1:nrow(pop), ncol = ceiling(nrow(pop)*3/3), 
+               lty = 1, legend = row.names(pop), title = "age class")
+        
+        barplot(pop[,ncol(pop)], col = 1:nrow(pop), 
                 main = "stable age distribution") 
         
     })
     
-    output$lifehistorytable = renderTable(
-        getLifeHistoryTable(input$n.stages, 
-                            input$births, 
-                            input$survival))
+   output$lesliematrix = renderTable(sim()$leslie)
 }
 
 
-#shinyApp(ui=ui, server=server)
+shinyApp(ui=ui, server=server)
