d9be0a5c72b3589245593dbf16aa6f4c3b85a476
EliGurarie <egurarie@umd.edu>
2023-03-02 15:47:29 -0500

    improved!

--- labs/lab7/logisticfitscript.R
+++ labs/lab7/logisticfitscript.R
@@ -1,10 +1,10 @@
-p <- read.csv("Paramecium.csv")
+# first load the paramecium.csv data
 N.logistic <- function(x, N0, K, r0) K/(1 + ((K - N0)/N0)*exp(-r0*x))
 
-plot(Nt~Day, data = p)
 # try different values!
 N0 <- 10
 K <- 150
-r0 <- 1.5
-plot(Nt~Day, data = p)
-curve(N.logistic(x, N0, K, r0), add = TRUE, col = 2)
+r0 <- 1
+
+plot(Nt ~ Day, data = paramecium)
+curve(N.logistic(x, N0, K, r0), add = TRUE, col = 2, lwd = 2)
