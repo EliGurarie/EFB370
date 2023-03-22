97ade639a747dc64ff40837b5c5b0f5bbae25d26
Elie Gurarie <egurarie@umd.edu>
2023-03-22 12:26:51 -0400

    Abalone and Metapopulation Modeler!

--- shinyapps/04_Metapopulations/metapopulations.R
+++ shinyapps/04_Metapopulations/metapopulations.R
@@ -3,56 +3,21 @@ require(shiny)
 require(shinyjs)
 library(shinyWidgets)
 
-runMetapopulation <- function(K = 20, N0 = 10, 
-                              e = .1, c = .2, 
-                              tmax = 1000,
-                              extinction = FALSE,
-                              colonization = FALSE){
-  metapop.matrix <- matrix(ncol = K, nrow = tmax)
-  metapop.matrix[1,] <- sample(c(rep(1, N0), rep(0, K-N0))) 
-  
-  for(i in 2:tmax){
-    N.then <-  metapop.matrix[i-1,]
-    f <- N.then / K
-    if(extinction == FALSE)
-      metapop.matrix[i,N.then == 1] <- rbinom(sum(N.then), size = 1, prob = 1-e) else 
-        metapop.matrix[i,N.then == 1] <- rbinom(sum(N.then), size = 1, prob = (1-f)*e)
-      if(colonization == FALSE)
-        metapop.matrix[i,N.then == 0] <- rbinom(sum(N.then == 0), size = 1, prob = c) else 
-          metapop.matrix[i,N.then == 0] <- rbinom(sum(N.then == 0), size = 1, prob = c*f)
-  }
-  
-  return(list(mp = metapop.matrix, 
-              Z =  runif(K) + 1i*runif(K),
-              pars =  c(e = e, c = c),
-              model = c(extinction, colonization)))
+source("metapopulation_functions.R")
+
+eval <- FALSE
+if(eval){
+
+  e <- .3; c <- .4 
+  sim <- runMetapopulation(K = 100, 
+                           extinction = "constant", 
+                           colonization = "external",
+                           N0 = 10, e = e, c = c, 
+                           tmax = 1000)
+  plotMP_timeseries(sim)
+  abline(h = (1-e/c)*100)
+  abline(h = (c/e)*100)
 }
-
-sim <- runMetapopulation(K = 100, extinction = FALSE, colonization = TRUE)
-
-plotMP_timeseries <- function(sim){
-  mp <- sim$mp
-  e <- sim$pars["e"]
-  c <- sim$pars["c"]
-  
-  rowSums(mp) %>% plot(type = "o", ylim = c(0,ncol(mp)), pch = 21, bg = "grey", 
-                       xlab = "Time", ylab = "Occupied sites", col = "darkgrey")
-  K <- ncol(mp)
-  
-  ex <- sim$model[1] == TRUE     # extinction process
-  cz <- sim$model[2] == TRUE     # colonization process
-  
-  
-  n.hat <- ifelse(!ex & !cz,  K*c/(c+e), 
-                  ifelse(!ex & cz, max(0,K * (1-e/c)), 
-                         ifelse(ex & !cz, min(K, K*c/e), NA)))
-  
-  linecols <- c("orange", "darkblue","darkred")
-  abline(h = c(0, n.hat, ncol(mp)), col = linecols, lwd = 2)
-  text(rep((nrow(mp)+1)/2, 3), c(0, n.hat, ncol(mp)), c("min", "theory", "max"), 
-       col = linecols, pos = 3, adj = 1, xpd = NA, font = 2)
-}
-
 
 ui <- fluidPage(
   
@@ -71,12 +36,12 @@ ui <- fluidPage(
     h3("Process model"),
     
     radioButtons("extinction", label = "Extinction process:",
-                 choices = list('Neutral' = FALSE, 'With rescue' = TRUE), 
-                 selected = FALSE),
+                 choices = list('Neutral' = "constant", 'With rescue' = "rescue"), 
+                 selected = "constant"),
     
     radioButtons("colonization", "Colonization process:", 
-                 choices = list('External' = FALSE, 'Internal' = TRUE),
-                 selected = FALSE), 
+                 choices = list('External' = "external", 'Internal' = "internal"),
+                 selected = "external"), 
     
     h3("Metapopulation setup"),
     
@@ -100,7 +65,7 @@ ui <- fluidPage(
     actionButton("go", "Refresh simulation")),
   
   fluidRow( 
-    column(width = 7, style='padding:10px', plotOutput("plots")))
+    column(width = 7, height = 7, style='padding:10px', plotOutput("plots", width = "600px", height = "500px")))
 )
 
 server <- function(input, output) {
@@ -114,11 +79,14 @@ server <- function(input, output) {
     mymp <- mysim$mp[1:input$tmax,]
     mysim$mp <- mymp
     
-    par(mfrow = c(1,2), tck = 0.01, bty = "l", las= 1, 
+    par(tck = 0.01, bty = "l", las= 1, 
         cex.lab = 1.5, mgp = c(1.25,.25,0))
+    
+    layout(rbind(c(1,1,2,2),c(4,3,3,5)))
     plotMP_timeseries(mysim)
     plot(mysim$Z, cex = 2, pch = 21, bg = mymp[nrow(mymp),], asp = 1, 
          xaxt = "n", yaxt = "n", xlab = "X", ylab = "Y")
+    plotMP_theory(mysim)
   })
 }
 
