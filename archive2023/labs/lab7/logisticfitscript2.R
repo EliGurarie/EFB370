7fb41aa938517985fcb6637bf401854d0f5a25e7
EliGurarie <egurarie@umd.edu>
2024-03-04 11:55:30 -0500

    Lab II.4 uploaded

--- archive2023/labs/lab7/logisticfitscript2.R
+++
@@ -1,5 +0,0 @@
-p <- read.csv("paramecium")
-N.logistic <- function(x, N0, K, r0) K/(1 + ((K - N0)/N0)*exp(-r0*x))
-
-nls(Nt ~ N.logistic(Day, N0, K, r0), data = p, 
-    start = list(N0 = 1, K = 200, r0 = 0.75))
