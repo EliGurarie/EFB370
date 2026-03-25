90d735c92a4f1891314967fbb7dba5ef8e596f00
Eliezer Gurarie <egurarie@esf.edu>
2026-03-25 12:06:24 -0400

    Getting into competition

---
+++ shinyapps/03_AgeStructuredGrowth/agestructuredgrowth_TasDevil.R
@@ -0,0 +1,97 @@
+# stages <- 1:3
+# fecundities <- c(0,1.5,.5)
+# mortalities <- c(.5,0,1)
+# start <- c(20,0,0)
+
+require(expm)
+require(gplots)
+require(kableExtra)
+require(plyr)
+require(magrittr)
+require(shinyMatrix)
+require(utils)
+
+
+runPop <- function(lifehistory, tmax){
+    
+    n.stages <- nrow(lifehistory)
+    births <- lifehistory[,"births"]
+    survival <- lifehistory[,"survival"]
+    
+    M <- matrix(0, nrow = n.stages, ncol = n.stages)
+    M[1,] <- births
+    for(i in 2:n.stages) M[i,i-1] <- survival[i-1]
+    row.names(M) <- row.names(lifehistory)
+    
+    time <- 0:tmax
+    pop <- sapply(time, function(t) {(M%^%t) %*% lifehistory[,"start"]})
+    row.names(pop) <- row.names(lifehistory) 
+    
+    return(list(pop = pop, leslie = M))
+}
+
+m <- rbind(c(0,.1,.5,.7),
+                    c(.7,0,0,0),
+                    c(0,.8,0,0),
+                    c(0,0,.8,.5))
+# colnames(m) <- c("start", "survival", "births")
+row.names(m) <- c("0", "1", "2", "3 +")
+ 
+ui <- fluidPage(
+    sidebarPanel(
+        h4("Life history parameters:"),
+        matrixInput(
+            "lifehistory",
+            value = m,
+            rows = list(extend = TRUE, 
+                        names = TRUE, 
+                        editableNames = TRUE, 
+                        delta = 2,
+                        delete = TRUE),
+            cols = list(names = TRUE, delta = 1),
+            class = "numeric"),
+            
+        numericInput(inputId = "tmax", 
+                     label = "Duration:", 
+                     value = 20),
+        h3(actionButton("button", "Update Life Table")),
+        h3(actionButton("go", "Run Simulation"))),
+    
+    mainPanel(
+        plotOutput("structuredgrowth", width = "100%", height = "350px"),
+        h3("Leslie Matrix"),
+        tableOutput("lesliematrix")
+    )
+)
+
+server <- function(input, output) {
+    sim <- eventReactive(input$go,
+                         runPop(input$lifehistory, 
+                                input$tmax))
+    
+    output$structuredgrowth = renderPlot({
+        
+        mysim <- sim()
+        pop <- mysim$pop
+        
+        palette(rich.colors(nrow(pop)))
+        par(mfrow = c(1,2), tck = 0.01, bty = "l", las= 1, 
+            cex.lab = 1.5, mgp = c(1.25,.25,0))
+        
+        layout(t(1:2), widths = c(2,1))
+        
+        matplot(t(pop), type = "o", pch = 19, lty = 1, xlab = "time", ylab = "N")
+        
+        legend("top", col = 1:nrow(pop), ncol = ceiling(nrow(pop)*3/3), 
+               lty = 1, legend = row.names(pop), title = "age class")
+        
+        barplot(pop[,ncol(pop)], col = 1:nrow(pop), 
+                main = "stable age distribution") 
+        
+    })
+    
+   output$lesliematrix = renderTable(sim()$leslie)
+}
+
+
+shinyApp(ui=ui, server=server)
---
+++ shinyapps/03_AgeStructuredGrowth/lesliematrix_TasDevil.R
@@ -0,0 +1,187 @@
+require(shiny)
+require(expm)
+require(gplots)
+require(plyr)
+require(magrittr)
+require(shinyMatrix)
+require(utils)
+require(shinyjs)
+library(shinyWidgets)
+library(DiagrammeR)
+source("leslie_diagram.R")
+
+runPop <- function(M, N0, tmax){
+  
+  time <- 0:tmax
+  pop <- sapply(time, function(t) {(M%^%t) %*% N0})
+  row.names(pop) <- row.names(M) 
+  
+  return(pop)
+}
+
+#M <- rbind(c(0,1.5,.5),c(.5,0,0),c(0,1,0))
+#N0 <- c(20,0,0) %>% t
+
+
+M <- rbind(c(0,.1,.5,.7),
+           c(.7,0,0,0),
+           c(0,.8,0,0),
+           c(0,0,.8,.5))
+# colnames(m) <- c("start", "survival", "births")
+N0 <- c(20,0,0,0) %>% t
+
+
+colnames(M) <- colnames(N0) <- row.names(M) <- c("0", "1", "2", "3 +")
+
+tidyM <- function(M){
+  blanks <- which(is.na(M), arr.ind = TRUE)
+  
+  if(nrow(blanks) != 0){
+    if(blanks[1] > blanks[2]){
+      M <- M[-blanks[1],]
+    } 
+    if(blanks[1] < blanks[2]){
+      M <- M[,-blanks[2]]
+    } 
+    if(blanks[1] == blanks[2]){
+      M <- M[-blanks[1],-blanks[2]]
+    }
+  }
+  return(M)
+}
+
+ui <- fluidPage(
+  
+  useShinyjs(),
+  setBackgroundColor("antiquewhite"),
+  tags$style(
+    "*, div {
+      font-family: Consolas;
+    }"
+  ),
+  
+  h1("Matrix Modeller 5000"),
+  
+  sidebarPanel(
+    h3("Enter Leslie matrix:"),
+    h5("Must be square, i.e. equal number of rows and columns:"),
+    matrixInput(
+      "lesliematrix",
+      value = M,
+      inputClass = "numeric",
+      rows = list(extend = TRUE, 
+                  delete = TRUE,
+                  copy = TRUE,
+                  names = TRUE,
+                  editableNames = TRUE),
+      cols = list(extend = TRUE, 
+                  paste = TRUE, 
+                  delete = TRUE, 
+                  names = TRUE),
+      class = "numeric"),
+    
+    h3("Enter initial population:"),
+    h5("Must be the same length as the Leslie matrix has columns:"),
+    matrixInput(
+      "N0",
+      value = N0,
+      inputClass = "numeric",
+      rows = list(extend = FALSE, n = 1),
+      cols = list(extend = TRUE, names = TRUE, delete = TRUE),
+      class = "numeric"),
+    
+    numericInput(inputId = "tmax", 
+                 label = "Duration:", 
+                 value = 20),
+    # h3(actionButton("button", "Update Life Table")),
+    actionButton("drawdiagram", "Draw diagram"),
+    actionButton("eigen", "Compute eigenvalues"),
+    actionButton("go", "Run simulation")),
+  
+  h3(actionButton("resetAll", "Clear everything")),
+  
+  mainPanel(fluidRow( 
+    column(width = 12, style='padding:10px', grVizOutput("diagram", height = "200px")), 
+    column(width = 12, style='padding:10px', align = "center", plotOutput("eigenvalues", height = "100px", width = "80%")),
+    column(width = 12, style='padding:10px', plotOutput("structuredgrowthplot", height = "350px"))
+  ))
+)
+
+server <- function(input, output) {
+  
+  sim <- eventReactive(input$go,
+                       runPop(tidyM(input$lesliematrix), 
+                              t(input$N0), input$tmax)
+  )
+  
+  diagram <- eventReactive(input$drawdiagram,
+                           leslie_diagram(tidyM(input$lesliematrix), 
+                                          stages = row.names(tidyM(input$lesliematrix)),
+                                          height = 300)
+  )
+  
+  
+  #M <- matrix(1:4, nrow = 2) %>% rbind(c(0,""))
+  
+  eigenplot <- eventReactive(input$eigen,{
+    M <- tidyM(input$lesliematrix)
+    
+    if(nrow(M) == ncol(M)){
+      eigenvalue <- eigen(M)$values[1]
+      eigenvector <- Re(eigen(M)$vectors[,1])
+      eigenvector <- eigenvector/sum(eigenvector)
+      eigenvectors <- paste0("{",paste(round(eigenvector, 3), collapse = ", "),"}")
+      
+      par(mar = c(0,0,4,0), bty = "n", mfrow = c(1,2), cex.main = 2)
+      plot(0, 0, type = "l", xaxt = "n", yaxt = "n",  main = expression("eigenvalue "~lambda~":"))
+      text(0, 0, round(Re(eigenvalue),3), cex = 2)
+      plot(0, 0, type = "l", xaxt = "n", yaxt = "n",  main = expression("eigenvector "~N^"*"~":"))
+      text(0, 0, eigenvectors, cex = 2*sqrt(3/length(eigenvector)))
+    } else{
+      par(mar = c(0,0,0,0))
+      image(1:nrow(M),1:ncol(M),M)
+      stop("non-square matrix - be sure you clear all empty rows and columns by clicking on the little X.")
+    }
+  })
+  
+  simplot <-  eventReactive(input$go, {
+    pop <- sim()
+    palette(rich.colors(nrow(pop)))
+    par(mfrow = c(1,2), tck = 0.01, bty = "l", las= 1, 
+        cex.lab = 1.5, mgp = c(1.25,.25,0))
+    
+    layout(t(1:2), widths = c(2,1))
+    matplot(t(pop), type = "o", pch = 19, lty = 1, xlab = "time", ylab = "N")
+    legend("top", col = 1:nrow(pop), ncol = ceiling(nrow(pop)*3/3), 
+           lty = 1, legend = row.names(pop), title = "age class")
+    barplot(pop[,ncol(pop)], col = 1:nrow(pop), 
+            main = "final distribution") 
+  })
+  
+  
+  observeEvent(input$go,{
+    output$structuredgrowthplot = renderPlot({simplot()})
+  })
+  
+  observeEvent(input$drawdiagram,{
+    output$diagram = renderGrViz({diagram()})
+  })
+  
+  observeEvent(input$eigen,{
+    output$eigenvalues = renderPlot({eigenplot()})
+  })
+  
+  observeEvent(input$resetAll, {
+    output$diagram <- renderGrViz({})
+    output$eigenvalues <- renderPlot({})
+    output$structuredgrowthplot <- renderPlot({})
+  })
+  
+  verbatimTextOutput("extinctionreport")
+}
+
+
+shinyApp(ui=ui, server=server)
+
+
+
